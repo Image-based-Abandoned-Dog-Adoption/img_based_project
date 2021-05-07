61ed240569d962af8fbecc07fee0f0398fe2d0f8
JungPhilPark <slavh96@naver.com>
2021-05-07 20:43:52 +0900

    spring-python link
    spring-python link

--- abandog.py
+++ abandog.py
@@ -35,9 +35,25 @@ conn = pymysql.connect(host='34.64.190.82', user='root', password='abandog', db=
 # 접속정보
 cur = conn.cursor(pymysql.cursors.DictCursor)
 # 커서생성
-sql = 'TRUNCATE `dog`'
-cur.execute(sql)
-sql = "INSERT INTO `dog` VALUES (%(age)s, %(careAddr)s, %(careNm)s, %(careTel)s, %(colorCd)s, %(popfile)s, %(kindCd)s, %(sexCd)s, %(neuterYn)s, cid) ON DUPLICATE KEY UPDATE cid = cid+1;"
+
+# 모든 컬럼 N인거 지울까말까 -> 마이페이지와 얘기해보기
+
+# 모든 protect 컬럼 N으로 만들기
+sql = 'UPDATE `dog` SET protect = %s'
+cur.execute(sql, 'n')
+conn.commit()
+
+for i in range(len(dict_data['response']['body']['items']['item'])):
+    # popfile을 통해서 사진하나하나의 특징벡터 얻기!
+    dict_data['response']['body']['items']['item'][i]['happenDt'] = "123"
+
+# 하나하나 보며 insert할지 updt할지 결정
+sql = "INSERT INTO `dog` (age, careAddr, careNm, careTel, color, img, kind, sex, neuter, specials, marks) VALUES (%(age)s, %(careAddr)s, %(careNm)s, %(careTel)s, %(colorCd)s, %(popfile)s, %(kindCd)s, %(sexCd)s, %(neuterYn)s, %(happenDt)s, %(specialMark)s) ON DUPLICATE KEY UPDATE `protect` = 'y';"
 cur.executemany(sql, dict_data['response']['body']['items']['item'])
 conn.commit()
+
+# 이제 protect = 'y'인 애들 가져와서 knn구성하기!!!!!!!!!!!!!!!
+# 
+#
+#
 conn.close()
---
+++ abandog/src/main/java/com/doglovers/abandog/dao/DogDAOImg.java
@@ -0,0 +1,16 @@
+package com.doglovers.abandog.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.doglovers.abandog.dto.DogDTO;
+import com.doglovers.abandog.dto.Pagination;
+
+public interface DogDAOImg {
+	int selectDogListNum();
+	int selectDogListNum2(Pagination pagination);
+	ArrayList<DogDTO> selectDogList(Pagination pagination);
+	DogDTO selectDog(int cid);
+	ArrayList<DogDTO> selectDogList_cids(String[] cids_arr);
+}
